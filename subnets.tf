f6a2262f61c48e46ae1bd37729d7043741bb80b1
SRUJANA ADAPA <adapa.s@northeastern.edu>
2025-02-24 14:48:36 -0500

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -6,7 +6,7 @@ data "aws_availability_zones" "available" {
 # Create Public Subnets
 resource "aws_subnet" "subnets_public" {
   count                   = var.total_public_subnets
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  =    aws_vpc.main.id
   cidr_block              = cidrsubnet(var.vpc_cidr_block, var.subnet_size, count.index)
   availability_zone       = data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]
   map_public_ip_on_launch = true
